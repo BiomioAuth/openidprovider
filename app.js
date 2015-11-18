1af963b9f0d031668a907e4cd5df3ec07c2826f2
Sergiy Dragunov <sergiy.dragunov@vakoms.com.ua>
2015-11-18 16:21:07 +0200

    logout

--- app.js
+++ app.js
@@ -203,7 +203,8 @@ app.get('/', function(req, res) {
 
 app.get('/login', auth.login());
 
-app.all('/logout', oidc.removetokens(), auth.logout(), function(req, res) {
+//app.all('/logout', oidc.removetokens(), auth.logout(), function(req, res)
+app.all('/logout', auth.logout(), function(req, res) {
   sessionStore.destroy(req.session.id, function (error, sess) {
     console.info('session destroy: ', error, sess);
   });
